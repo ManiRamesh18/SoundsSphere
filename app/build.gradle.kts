e32943aafbfaac756b786b383f01388928bc0ecd
Mani R <mani1832r@gmail.com>
2024-01-02 17:48:09 +0530

    Done Some Ui CHnages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,4 +103,7 @@ dependencies {
 
     implementation ("androidx.core:core-splashscreen:1.0.1")
 
+    implementation ("androidx.compose.ui:ui:1.2.0-beta02")
+    implementation ("androidx.compose.material:material:1.2.0-beta02")
+
 }
