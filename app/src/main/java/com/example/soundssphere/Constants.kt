bfb42199820e56b85d48d91b5f27597890ce2e0d
Mani R <mani1832r@gmail.com>
2024-01-05 16:04:30 +0530

    Done changes

--- app/src/main/java/com/example/soundssphere/Constants.kt
+++ app/src/main/java/com/example/soundssphere/Constants.kt
@@ -3,11 +3,11 @@ package com.example.soundssphere
 object Constants
 {
     const val ACCOUNTS_URL ="https://accounts.spotify.com/api/token"
-    const val BASE_URL = "https://api.spotify.com/v1/"
+    const val BASE_URL = "https://api.spotify.com/v1/browse/"
     const val CLIENT_ID = "e50cfd48e2244da090c8adbfbace241a"
     const val CLIENT_SECRET = "68b48d2eb14e443e91868cf3a0a9b95c"
     const val GRANT_TYPE = "client_credentials"
 
-    val accessToken : String = "BQA87kMujcKFyXbX6T-d4G5DCTp28oJWJWzHjBx1C2NYwjVMdsh_L60ih7ikPdto5LYsmYbRHJd5J5dSDfyg2rtjpvwxjn5WCnmNo22JSQO1-F6QjyA"
+    val accessToken : String = "BQCGagQyKjIbAtGtu9n5o-slfbfhxBPLJelA-MCMcoYPiOI09t3-L9EyOJBvU32oWHVV7C_soJlOGuDz3R4MiVVIfDuUMrIdBJjUBi6Iyam8F8CVJi0"
     val expiresAt : Int =0
 }
